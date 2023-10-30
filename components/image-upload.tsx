dd1b3f1956def1c4a835481ab4feff7f3a1bc9d8
wu649782027 <15151851516@163.com>
2023-10-30 19:23:40 +0800

    temp

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import {FC, useEffect, useState} from "react";
+import {CldUploadButton} from "next-cloudinary";
+import Image from "next/image";
 
 interface ImageUploadProps {
   value: string
@@ -13,12 +15,26 @@ const ImageUpload: FC<ImageUploadProps> = ({value, onChange, disabled}) => {
   useEffect(() => {
     setIsMounted(true)
   }, [])
-  if(!isMounted){
+  if (!isMounted) {
     return null
   }
   return (
     <div className={"space-y-4 w-full flex flex-col justify-center items-center"}>
-
+      <CldUploadButton
+        onUpload={(results: any) => onChange(results.info.secure_url)}
+        options={{
+          maxFiles: 1
+        }}
+        uploadPreset={"fob55gc7"}
+      >
+        <div className={`p-4 border-4 border-dashed border-primary/10 rounded-lg 
+        hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center`}>
+          <div className={"relative h-40 w-40"}>
+            <Image className={"rounded-lg object-cover"} src={value || "/placeholder.svg"} alt={"Upload"}
+                   fill/>
+          </div>
+        </div>
+      </CldUploadButton>
     </div>
   );
 }
