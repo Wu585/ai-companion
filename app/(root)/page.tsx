a5bec47212906797029520f50a5290bb07177db9
wu649782027 <15151851516@163.com>
2023-10-31 18:00:54 +0800

    temp

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,8 @@
 import SearchInput from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
 import Categories from "@/components/categories";
+import {FC} from "react";
+import Companions from "@/components/companions";
 
 interface RootPageProps {
   searchParams: {
@@ -9,12 +11,32 @@ interface RootPageProps {
   }
 }
 
-const RootPage = async () => {
+const RootPage: FC<RootPageProps> = async ({searchParams}) => {
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name
+      }
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+    /*include: {
+      _count: {
+        select: {
+          messages: true
+        }
+      }
+    }*/
+  })
+
   const categories = await prismadb.category.findMany()
   return (
     <div className={"h-full p-4 space-y-2"}>
       <SearchInput/>
       <Categories data={categories}/>
+      <Companions data={data}/>
     </div>
   );
 }
